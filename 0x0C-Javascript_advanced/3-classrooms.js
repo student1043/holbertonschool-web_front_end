7bd677315f2e7713ab8c6089bbb762567df8bed3
student1043 <1043@holbertonschool.com>
2020-09-21 23:14:38 +0100

    Fixing output

--- 0x0C-Javascript_advanced/3-classrooms.js
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -6,7 +6,7 @@ function createClassRoom(numbersOfStudents) {
     }
     let students = [];
     for (let i = 0; i < numbersOfStudents; i++) {
-        students.push(studentSeat(i + 1));
+        students[i] = studentSeat(i + 1);
     }
     return(students);
 }
